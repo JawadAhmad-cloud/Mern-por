599ca862e1bd294e25f6e29082f65f7d07ff5087
JawadAhmad <gptchat076@gmail.com>
2025-10-21 19:36:18 +0500

    setup bootstrap and also reactrouterdom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
+import Navbar from "../components/Navbar";
+import About from "../components/About";
 
 const App = () => {
   return (
-    <div>
-      <h1>App inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
